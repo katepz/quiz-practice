63a6806af3953f91e953c7d5c5ced073c3cceaf7
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-10-19 15:33:43 +0000

    Add: style.css, Start: js coding

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * to start the game when we click on start button
+ */
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    console.log("started");
+}
+/**
+ * to set the next question when we click on next button
+ */
+function setNextQuestion() {
+
+}
+
+/**
+ * select an answer
+ */
+
+function selectAnswer() {
+
+}
